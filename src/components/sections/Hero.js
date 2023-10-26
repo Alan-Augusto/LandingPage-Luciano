d56ced5bfd8f9ecb2fdd4f7943020b87740a2fc2
alan-augusto <augustoalan56@gmail.com>
2023-10-26 17:58:10 -0300

    ajuste hero section

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,12 +1,23 @@
 import "./Section.css";
 import "./Hero.css";
 import React from "react";
+import HeroIMmage from "./assets/hero.png"
 
-function Hero(){
-    return(
-        <section className="section" id="hero">
-            Hero
-        </section>
-    );
+function Hero() {
+  return (
+    <section className="section" id="hero">
+      <div id="hero-container">
+        <div id="hero-actions">
+          <h1>Defesa Trabalhista Exclusiva Para Empregados</h1>
+          <p>cada cliente é único e seu caso é nosso compromisso</p>
+          <button>Falar com Dr. Lciano</button>
+        </div>
+
+        <div id="hero-image">
+          <img src={HeroIMmage} />
+        </div>
+      </div>
+    </section>
+  );
 }
-export default Hero
+export default Hero;
