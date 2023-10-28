0970ed5229a061ddcb8660a8937268ff3c10575a
alan-augusto <augustoalan56@gmail.com>
2023-10-28 15:49:47 -0300

    ajustes finos HeroSection e Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,30 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import {sectionIds} from "./SectionIds"
+import { sectionIds } from "./SectionIds"
 import './Header.css';
 import { Link } from 'react-router-dom';
 
 function Header() {
 
   const [activeLink, setActiveLink] = useState("hero");
-  const [isScrolled, setIsScrolled] = useState(false)
+  const [isScrolled, setIsScrolled] = useState(false);
 
-  const scrollToSection = (sectionId) =>{
-    const element=document.getElementById(sectionId);
-    if(element){
+  const scrollToSection = (sectionId) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
       const marginTop = 0;
-      
-      const scrollToY = element.getBoundingClientRect().top + window.scrollY-marginTop;
-
-      window.scrollTo({top: scrollToY, behavior:"smooth"});
-    }  
+      const scrollToY = element.getBoundingClientRect().top + window.scrollY - marginTop;
+      window.scrollTo({ top: scrollToY, behavior: "smooth" });
+    }
   }
 
   const determineActiveSection = () => {
-    for(let i=sectionIds.length-1; i>=0; i--){
+    for (let i = sectionIds.length - 1; i >= 0; i--) {
       const section = document.getElementById(sectionIds[i]);
-      if(section){
+      if (section) {
         const rect = section.getBoundingClientRect();
-        if(rect.top<=120 && rect.bottom >= 120){
+        if (rect.top <= 120 && rect.bottom >= 120) {
           setActiveLink(sectionIds[i]);
           break;
         }
@@ -32,12 +30,17 @@ function Header() {
     }
   }
 
-  useEffect(()=>{
-    const handleScroll = ()=>{
-      if(window.scrollY > 300){
+  const handleSelectChange = (event) => {
+    const selectedSection = event.target.value;
+    setActiveLink(selectedSection);
+    scrollToSection(selectedSection);
+  }
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
         setIsScrolled(true);
-      }
-      else{
+      } else {
         setIsScrolled(false);
       }
 
@@ -46,14 +49,14 @@ function Header() {
 
     window.addEventListener('scroll', handleScroll);
 
-    return()=>{
+    return () => {
       window.removeEventListener('scroll', handleScroll);
     }
-  },[]);
+  }, []);
 
   return (
     <div className="Header">
-      <nav className={isScrolled ? "scrolled" : "" }>
+      <nav className={isScrolled ? "scrolled" : ""}>
         <div className='container'>
           <div className='row'>
             <div className='logo'>
@@ -62,13 +65,23 @@ function Header() {
 
             <ul className='menu-bar'>
               {
-                sectionIds.map((sectionId, i) =>(
-                  <li key={i} onClick={()=>scrollToSection(sectionId)}>
+                sectionIds.map((sectionId, i) => (
+                  <li key={i} onClick={() => scrollToSection(sectionId)}>
                     <Link to="/" className={activeLink === sectionId ? "active" : ""}>{sectionId}</Link>
                   </li>
                 ))
               }
             </ul>
+
+            <div className='mobile-menu-bar'>
+              <select onChange={handleSelectChange} value={activeLink}>
+                {sectionIds.map((section, i) => (
+                  <option key={i} value={section}>
+                    {section}
+                  </option>
+                ))}
+              </select>
+            </div>
           </div>
         </div>
       </nav>
